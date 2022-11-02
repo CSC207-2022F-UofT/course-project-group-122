59bab2dfb922f37d3d99ef4d1cbe16ac4275d1fc
annaxu <annaxu44@gmail.com>
2022-11-01 22:20:16 -0400

    Added addToListStudies and removeFromListStudies for the EnrollParticipant and CloseStudy use case. Also wrote a UnitTest for Factory. It ins't working because the code doesn't have Class Study merged to it yet.

---
+++ src/test/Entities/UserFactoryTest.java
@@ -0,0 +1,26 @@
+package Entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserFactoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void create() throws Exception {
+        UserFactory userFactory = new UserFactory();
+        User newUser = userFactory.create("Participant", "annarxy", "anna");
+        assertTrue(newUser.getClass().equals(Participant.class));
+    }
+}
