fd2e89d366bf118a082977ae7cbca780949b6ad0
Jielin Yang <89996402+j-y26@users.noreply.github.com>
2022-11-19 14:46:49 -0500

    use request model for modify study parameters

--- src/main/java/use_cases/modify_study_parameters/ModifyStudyParameterController.java
+++ src/main/java/use_cases/modify_study_parameters/ModifyStudyParameterController.java
@@ -1,45 +1,44 @@
 package use_cases.modify_study_parameters;
 
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * The controller for modifying the parameters of a study.
  */
 public class ModifyStudyParameterController {
 
     /**
-     * The interactor of the use case.
+     * The input boundary for modifying the parameters of a study.
      */
-    private final ModifyStudyParameterInputBoundary modifyStudyParameterInteractor = new ModifyStudyParameterInteractor();
+    private ModifyStudyParameterInputBoundary modifyStudyParameterInteractor = new ModifyStudyParameterInteractor();
 
 
     /**
-     * Returns the list of potential stratification variables for the study.
+     * Modify the parameters of a study.
+     * @param requestModel      The request model containing the new parameters of the study.
+     */
+    public void modifystudyParameters(@NotNull ModifyStudyParameterRequestModel requestModel) {
+        int studyId = requestModel.getStudyId();
+        String studyName = requestModel.getStudyName();
+        String studyDescription = requestModel.getStudyDescription();
+        int targetSize = requestModel.getStudyTargetSize();
+        String studyType = requestModel.getStudyType();
+        int numGroups = requestModel.getNumGroups();
+        String[] groupNames = requestModel.getGroupNames();
+
+        modifyStudyParameterInteractor.modifyStudyBasicParameters(studyId, studyName, studyDescription, targetSize);
+        modifyStudyParameterInteractor.modifyStudyType(studyId, studyType);
+        modifyStudyParameterInteractor.modifyStudyGrouping(studyId, numGroups, groupNames);
+    }
+
+
+    /**
+     * Checks the potential stratification variables for the study.
      * @param studyID   The ID of the study.
      */
     public void checkPotentialStratificationVariables(int studyID) {
         modifyStudyParameterInteractor.checkPotentialStratificationVariables(studyID);
-    }
-
-
-    /**
-     * Modifies the basic parameters of the study.
-     * @param studyId               The ID of the study.
-     * @param studyName             The new name of the study.
-     * @param studyDescription      The new description of the study.
-     * @param targetSize            The new target size of the study.
-     */
-    public void modifyStudyBasicParameters(int studyId, String studyName, String studyDescription, int targetSize) {
-        modifyStudyParameterInteractor.modifyStudyBasicParameters(studyId, studyName, studyDescription, targetSize);
-    }
-
-
-    /**
-     * Modifies the type of the study.
-     * @param studyId           The ID of the study.
-     * @param studyType         The new type of the study.
-     */
-    public void modifyStudyType(int studyId, String studyType) {
-        modifyStudyParameterInteractor.modifyStudyType(studyId, studyType);
     }
 
 
@@ -58,19 +57,7 @@ public class ModifyStudyParameterController {
      * @param studyId                   The ID of the study.
      * @param stratificationVariable    The new stratification variable of the study.
      */
-    public void modifyStratificationVariables(int studyId, String stratificationVariable) {
+    public void modifyStudyStratification(int studyId, String stratificationVariable) {
         modifyStudyParameterInteractor.modifyStudyStratification(studyId, stratificationVariable);
     }
-
-
-    /**
-     * Modifies the grouping of the study
-     * @param studyId       The ID of the study.
-     * @param groupSize     The new group size of the study.
-     * @param groupNames    The new group names of the study.
-     */
-    public void modifyStudyGrouping(int studyId, int groupSize, String[] groupNames) {
-        modifyStudyParameterInteractor.modifyStudyGrouping(studyId, groupSize, groupNames);
-    }
-
 }
---
+++ src/main/java/use_cases/modify_study_parameters/ModifyStudyParameterRequestModel.java
@@ -0,0 +1,155 @@
+package use_cases.modify_study_parameters;
+
+public class ModifyStudyParameterRequestModel {
+
+    /**
+     * The id of the study to modify.
+     */
+    private int studyId;
+
+    /**
+     * The new name of the study.
+     */
+    private String studyName;
+
+    /**
+     * The new description of the study.
+     */
+    private String studyDescription;
+
+    /**
+     * The target number of participants for the study.
+     */
+    private int studyTargetSize;
+
+    /**
+     * The type of the study.
+     * Has to be one of the following:
+     * "General",
+     * "Randomized"
+     */
+    private String studyType;
+
+    /**
+     * The number of groups in the study.
+     */
+    private int numGroups;
+
+    /**
+     * The names of the groups in the study.
+     * The number of names has to be equal to the number of groups.
+     */
+    private String[] groupNames;
+
+
+    /**
+     * Creates a new ModifyStudyParameterRequestModel.
+     * @param studyId           The id of the study to modify.
+     * @param studyName         The new name of the study.
+     * @param studyDescription  The new description of the study.
+     */
+    public ModifyStudyParameterRequestModel(int studyId, String studyName, String studyDescription) {
+        this.studyId = studyId;
+        this.studyName = studyName;
+        this.studyDescription = studyDescription;
+    }
+
+
+    /**
+     * Set the target size of the study.
+     * @param studyTargetSize
+     */
+    public void setStudyTargetSize(int studyTargetSize) {
+        this.studyTargetSize = studyTargetSize;
+    }
+
+
+    /**
+     * Set the type of the study.
+     * @param studyType The type of the study.
+     */
+    public void setStudyType(String studyType) {
+        this.studyType = studyType;
+    }
+
+    /**
+     * Set the number of groups in the study.
+     * @param numGroups     The number of groups in the study.
+     */
+    public void setNumGroups(int numGroups) {
+        this.numGroups = numGroups;
+    }
+
+
+    /**
+     * Set the names of the groups in the study.
+     *
+     * @param groupNames    The names of the groups in the study.
+     */
+    public void setGroupNames(String[] groupNames) {
+        this.groupNames = groupNames;
+    }
+
+
+    /**
+     * Get the id of the study to modify.
+     * @return  The id of the study to modify.
+     */
+    public int getStudyId() {
+        return studyId;
+    }
+
+
+    /**
+     * Get the new name of the study.
+     * @return  The new name of the study.
+     */
+    public String getStudyName() {
+        return studyName;
+    }
+
+
+    /**
+     * Get the new description of the study.
+     * @return  The new description of the study.
+     */
+    public String getStudyDescription() {
+        return studyDescription;
+    }
+
+
+    /**
+     * Get the target number of participants for the study.
+     * @return  The target number of participants for the study.
+     */
+    public int getStudyTargetSize() {
+        return studyTargetSize;
+    }
+
+
+    /**
+     * Get the type of the study.
+     * @return  The type of the study.
+     */
+    public String getStudyType() {
+        return studyType;
+    }
+
+
+    /**
+     * Get the number of groups in the study.
+     * @return  The number of groups in the study.
+     */
+    public int getNumGroups() {
+        return numGroups;
+    }
+
+
+    /**
+     * Get the names of the groups in the study.
+     * @return  The names of the groups in the study.
+     */
+    public String[] getGroupNames() {
+        return groupNames;
+    }
+}
