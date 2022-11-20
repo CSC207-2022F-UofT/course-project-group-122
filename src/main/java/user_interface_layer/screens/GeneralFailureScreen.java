6a368b53784458e85f38e158b65e763816814d28
Jielin Yang <89996402+j-y26@users.noreply.github.com>
2022-11-20 00:22:51 -0500

    general screens

---
+++ src/main/java/user_interface_layer/screens/GeneralFailureScreen.java
@@ -0,0 +1,10 @@
+package user_interface_layer.screens;
+
+import javax.swing.*;
+
+public class GeneralFailureScreen {
+
+    public GeneralFailureScreen(String FailureMessage) {
+        JOptionPane.showMessageDialog(null, FailureMessage);
+    }
+}
