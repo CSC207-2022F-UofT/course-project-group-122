d7b61015b2c419078ecb986832980303ead3f958
Jielin Yang <89996402+j-y26@users.noreply.github.com>
2022-11-20 20:39:48 -0500

    Delete src/main/java/use_cases/add_researcher_to_study_data_request directory

--- src/main/java/use_cases/add_researcher_to_study_data_request/AddResearcherStudyDataRequestController.java
+++
@@ -1,6 +0,0 @@
-package use_cases.add_researcher_to_study_data_request;
-
-public class AddResearcherStudyDataRequestController {
-    public void addResearcherToStudyRequest(int researcherIdInt, int studyId) {
-    }
-}
