6fa0b43e55ac2d2869b2ec8f56eacd59c7c2257b
Tedward765 <teddyleung123@gmail.com>
2022-11-21 21:12:25 -0500

    added serialization functionality

--- src/main/java/use_cases/user_log_out/UserLogOutInputBoundary.java
+++ src/main/java/use_cases/user_log_out/UserLogOutInputBoundary.java
@@ -1,9 +1,17 @@
 package use_cases.user_log_out;
 
+import entities.Study;
+import entities.StudyPool;
+import entities.UserPool;
+import use_cases.participant_enroller.RandomGroupGeneratorManager;
+
 public interface UserLogOutInputBoundary {
 
     /**
      * Log out the user and save the current state of data
      */
     void logOut();
+
+    void saveData(UserPool uP, StudyPool sP, RandomGroupGeneratorManager rGGM);
+
 }
--- src/main/java/use_cases/user_log_out/UserLogOutInteractor.java
+++ src/main/java/use_cases/user_log_out/UserLogOutInteractor.java
@@ -1,5 +1,10 @@
 package use_cases.user_log_out;
 
+import entities.StudyPool;
+import entities.UserPool;
+import use_cases.participant_enroller.RandomGroupGeneratorManager;
+import use_cases.serializer.Serializer;
+
 public class UserLogOutInteractor implements UserLogOutInputBoundary {
 
 
@@ -11,6 +16,23 @@ public class UserLogOutInteractor implements UserLogOutInputBoundary {
     @Override
     public void logOut() {
         userLogOutPresenter.logOut();
+    }
+
+    /**
+     * Serializes the following objects
+     *
+     * @param uP a UserPool object
+     * @param sP a StudyPool object
+     * @param rGGM a RandomGroupGeneratorManager object
+     */
+
+    @Override
+    public void saveData(UserPool uP, StudyPool sP, RandomGroupGeneratorManager rGGM) {
+        Serializer s = new Serializer();
+
+        s.saveObject(uP);
+        s.saveObject(sP);
+        s.saveObject(rGGM);
     }
 
     /**
