bc1737e2c6cfde670b1f2c6c0774cf6f83c4275f
xustell <stella.xu@mail.utoronto.ca>
2022-11-21 20:52:07 -0500

    finished assign_questionnaire use case and close_ questionnaire use case

--- src/main/java/use_cases/assign_questionnaire/AssignQuestionnaireController.java
+++ src/main/java/use_cases/assign_questionnaire/AssignQuestionnaireController.java
@@ -1,12 +1,21 @@
 package use_cases.assign_questionnaire;
 
 public class AssignQuestionnaireController {
-    public void assignQuestionnaireToGroup(int researchId, int studyId, String groupName) {
+
+    private assignquestionnaireinputboundary assignquestionnaireinteractor;
+
+    public void setassignquestionnaireinteractor(assignquestionnaireinputboundary assignquestionnaireinteractor) {
+        this.assignquestionnaireinteractor = assignquestionnaireinteractor;
+    }
+    public void assignQuestionnaireToGroup(int questionnaireId, int studyId, String group) {
+        assignquestionnaireinteractor.assignToGroup(questionnaireId, group, studyId);
     }
 
-    public void assignQuestionnaireToAll(int researchId, int studyId) {
+    public void assignQuestionnaireToAll(int questionnaireId, int studyId) {
+        assignquestionnaireinteractor.assigntoall(questionnaireId, studyId);
     }
 
     public void assignQuestionnaireToIndividual(int researchId, int studyId, int participant) {
+        assignquestionnaireinteractor.assigntoParticipant(researchId, studyId, participant);
     }
 }
