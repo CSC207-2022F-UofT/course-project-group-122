f4e9f1ea4d9324c6631baf717fae0fc9880ac6c8
Jerryfish0807 <goodddddd226@gmail.com>
2022-11-30 00:18:09 -0500

    EntitiesTest1.1(Based on the renewal of Study Class, no implementation yet)

---
+++ src/test/java/entities/ConsentFormTest.java
@@ -0,0 +1,54 @@
+package entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsentFormTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getStudy() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void setContent() {
+    }
+
+    @Test
+    void getStudyDescription() {
+    }
+
+    @Test
+    void setStudyDescription() {
+    }
+
+    @Test
+    void getRisksAndBenefits() {
+    }
+
+    @Test
+    void setRisksAndBenefits() {
+    }
+
+    @Test
+    void getParticipantRights() {
+    }
+
+    @Test
+    void setParticipantRights() {
+    }
+}
