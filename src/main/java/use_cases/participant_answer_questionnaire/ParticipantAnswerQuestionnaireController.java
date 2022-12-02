cd0621e2eda6c5cbceb121a3553e100466b9e0bf
annaxu <annaxu44@gmail.com>
2022-12-02 17:31:41 -0500

    Fixed conflict from main, because some of the files were repeated and they were old files

--- src/main/java/use_cases/participant_answer_questionnaire/ParticipantAnswerQuestionnaireController.java
+++
@@ -1,8 +0,0 @@
-package use_cases.participant_answer_questionnaire;
-
-import java.util.HashMap;
-
-public class ParticipantAnswerQuestionnaireController {
-    public void participantAnswerQuestionnaireRequest(int participantID, int questionnaireID, int studyID, HashMap<String, String[]> answers) {
-    }
-}
--- src/main/java/use_cases/remove_researcher_from_study/RemoveResearcherFromStudyController.java
+++
@@ -1,6 +0,0 @@
-package use_cases.remove_researcher_from_study;
-
-public class RemoveResearcherFromStudyController {
-    public void removeResearcherFromStudyRequest(int researcherId, int studyID) {
-    }
-}
