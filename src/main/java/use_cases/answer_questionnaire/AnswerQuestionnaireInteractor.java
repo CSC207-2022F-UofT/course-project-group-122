7c0de721bba914e137bc0c7ab3593c86363523b6
annaxu <annaxu44@gmail.com>
2022-12-04 23:43:46 -0500

    bug fixes

--- src/main/java/use_cases/answer_questionnaire/AnswerQuestionnaireInteractor.java
+++ src/main/java/use_cases/answer_questionnaire/AnswerQuestionnaireInteractor.java
@@ -37,10 +37,6 @@ public class AnswerQuestionnaireInteractor implements AnswerQuestionnaireInputBo
         Questionnaire questionnaire = FetchId.getQuestionnaire(questionnaireId, studyId);
         User modifier = FetchId.getUser(modifierId);
         Participant participant = (Participant) FetchId.getUser(participantId);
-        if (participant.getCompletedQuestionnaires().contains(questionnaire)) {
-            answerQuestionnairePresenter.presentAnswerQuestionnaireFailure(
-                    "Participant has already completed this questionnaire.");
-        }
         Study study = FetchId.getStudy(studyId);
         assert questionnaire != study.getEligibilityQuestionnaire();
         Answer newAnswer = answer(questionnaire, participant, modifier, study, answers, numQuestions);
@@ -73,16 +69,17 @@ public class AnswerQuestionnaireInteractor implements AnswerQuestionnaireInputBo
         if (participant.hasCompletedEligibilityQuestionnaire()) {
             answerQuestionnairePresenter.presentAnswerQuestionnaireFailure(
                     "Participant has already completed eligibility questionnaire.");
+        } else {
+            Study study = FetchId.getStudy(studyId);
+            assert Objects.equals(questionnaire, study.getEligibilityQuestionnaire());
+            Answer newAnswer = answer(questionnaire, participant, modifier, study, answers, numQuestions);
+            assert newAnswer != null;
+            VersionedAnswer newVersionedAnswer = newAnswer.getCurrentVersion();
+            participant.setEligibilityQuestionnaireAnswer(newAnswer);
+            String time  = newVersionedAnswer.getTimeOfModification();
+            answerQuestionnairePresenter.presentAnswerQuestionnaireSuccess(participantId, modifierId,
+                    questionnaireId, time);
         }
-        Study study = FetchId.getStudy(studyId);
-        assert Objects.equals(questionnaire, study.getEligibilityQuestionnaire());
-        Answer newAnswer = answer(questionnaire, participant, modifier, study, answers, numQuestions);
-        assert newAnswer != null;
-        VersionedAnswer newVersionedAnswer = newAnswer.getCurrentVersion();
-        participant.setEligibilityQuestionnaireAnswer(newAnswer);
-        String time  = newVersionedAnswer.getTimeOfModification();
-        answerQuestionnairePresenter.presentAnswerQuestionnaireSuccess(participantId, modifierId,
-                questionnaireId, time);
     }
 
 
