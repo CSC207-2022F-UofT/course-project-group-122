1dbdcbade813825faaa951fae381ecda506a1a7e
annaxu <annaxu44@gmail.com>
2022-12-08 19:15:38 -0500

    wrote the javaDoc for the screen manager

--- src/main/java/user_interface_layer/presenter_manager/ScreenManager.java
+++ src/main/java/user_interface_layer/presenter_manager/ScreenManager.java
@@ -33,49 +33,163 @@ import javax.swing.*;
  * The class that manages all the screens.
  */
 public class ScreenManager {
+
+    /**
+     * The instance of the current screen.
+     */
     private JFrame currentScreen;
+
+    /**
+     * The instance of the register screen.
+     */
     private UserRegisterScreen userRegisterScreen;
+
+    /**
+     * The instance of the log in screen.
+     */
     private UserLogInScreen logInScreen;
+
+    /**
+     * The instance of the sign up screen.
+     */
     private UserSignUpScreen signUpScreen;
+
+    /**
+     * The instance of the screen for a participant that is not part of any study.
+     */
     private ParticipantNoStudyScreen participantNoStudyScreen;
+
+    /**
+     * The instance of the screen for a participant that is part of a study.
+     */
     private ParticipantHomeScreen participantHomeScreen;
+
+    /**
+     * The instance of the study screen for a researcher.
+     */
     private ResearcherStudyScreen researcherStudiesScreen;
+
+    /**
+     * The instance of the screen for a researcher to create a study.
+     */
     private CreateStudyInputScreen createStudyInputScreen;
+
+    /**
+     * The instance of the screen for a researcher to see all the versioned answer of a questionnaire answered by
+     * a participant.
+     */
     private AllVersionsScreen allVersionsScreen;
+
+/**
+     * The instance of the screen for a researcher requesting to see the screen of a participant.
+     */
     private ResearcherRequestParticipantScreen researcherRequestParticipantScreen;
+
+    /**
+     * The instance of the screen for a researcher to create a questionnaire.
+     */
     private CreateQuestionnaireInputsScreen createQuestionnaireInputsScreen;
+
+    /**
+     * The instance of the screen for a researcher to check a questionnaire.
+     */
     private CheckQuestionnaireScreen checkQuestionnaireScreen;
+
+    /**
+     * The instance of the screen for a researcher to choose the stratification variable.
+     */
     private ChooseStratificationScreen chooseStratificationScreen;
+
+    /**
+     * The instance of the screen for a researcher to edit a questionnaire.
+     */
     private EditQuestionnaireScreen editQuestionnaireScreen;
+
+    /**
+     * The instance of the screen for a researcher to edit a study.
+     */
     private EditStudyScreen editStudyScreen;
+
+    /**
+     * The instance of the screen for a researcher or participant to answer a questionnaire.
+     */
     private UserAnswerQuestionnairesScreen userAnswerQuestionnairesScreen;
+
+    /**
+     * The instance of the screen for a researcher to edit the answers of a questionnaire.
+     */
     private EditQuestionnaireAnswers editQuestionnaireAnswers;
+
+    /**
+     * The instance of the screen for a researcher to see the data log of a study.
+     */
     private StudyDataLogScreen studyDataLogScreen;
+
+    /**
+     * The instance of the screen for a researcher to see the information of a researcher they want to enroll in the
+     * study.
+     */
     private ShowResearcherInfoScreen showResearcherInfoScreen;
+
+    /**
+     * The instance of the screen for a researcher to see the information of a participant they want to enroll in the
+     * study.
+     */
     private ShowParticipantInfoScreen showParticipantInfoScreen;
 
-
+    /**
+     * The instance of the screen for a researcher to see the groups they wabt to assign a participant to.
+     */
     private ChooseGroupToAssignScreen chooseGroupToAssignScreen;
 
     /**
      * The consent form screen.
      */
     private ConsentFormScreen consentFormScreen;
+
+/**
+     * The screen for creating a consent form.
+     */
     private CreateConsentFormScreen createConsentFormScreen;
+
+    /**
+     * The screen for a researcher to see the consent form.
+     */
     private ConsentFormScreenForReview consentFormScreenForReview;
+
+/**
+     * The screen for a researcher to confirm the assignment of a questionnaire to a participant.
+     */
     private ShowParticipantInfoForQAssignment showParticipantInfoForQAssignment;
+
+    /**
+     * The screen for a researcher to check the versioned answer of a questionnaire.
+     */
     private CheckVersionedAnswer checkVersionedAnswer;
 
+    /**
+     * The constructor of the class.
+     */
     public ScreenManager() {
     }
 
+    /**
+     * The method returns the instance of the current screen.
+     */
     public JFrame getCurrentScreen() {
         return currentScreen;
     }
 
+    /**
+     * The method sets the current screen.
+     */
     public void setCurrentScreen(JFrame currentScreen) {
         this.currentScreen = currentScreen;
     }
+
+    /**
+     * The method sets the instance of the register screen.
+     */
     public void setUserRegisterScreen(UserRegisterScreen userRegisterScreen) {
         if (this.userRegisterScreen != null) {
             this.userRegisterScreen.dispose();
@@ -83,6 +197,9 @@ public class ScreenManager {
         this.userRegisterScreen = userRegisterScreen;
     }
 
+    /**
+     * The method sets the instance of the log in screen.
+     */
     public void setLogInScreen(UserLogInScreen logInScreen) {
         if (this.logInScreen != null) {
             this.logInScreen.dispose();
@@ -90,6 +207,9 @@ public class ScreenManager {
         this.logInScreen = logInScreen;
     }
 
+    /**
+     * The method sets the instance of the sign-up screen.
+     */
     public void setSignUpScreen(UserSignUpScreen signUpScreen) {
         if (this.signUpScreen != null) {
             this.signUpScreen.dispose();
@@ -97,6 +217,9 @@ public class ScreenManager {
         this.signUpScreen = signUpScreen;
     }
 
+    /**
+     * The method sets the instance of the screen for a participant that is not part of any study.
+     */
     public void setParticipantNoStudyScreen(ParticipantNoStudyScreen screen) {
         if (this.participantNoStudyScreen != null) {
             this.participantNoStudyScreen.dispose();
@@ -104,30 +227,45 @@ public class ScreenManager {
         this.participantNoStudyScreen = screen;
     }
 
+    /**
+     * The method that updates the current screen and disposes of the previos screen
+     */
     public void updateCurrentScreen(JFrame screen) {
-        if (currentScreen == null) {
-            setCurrentScreen(screen);
-            getCurrentScreen().setVisible(true);
-        } else {
+        if (currentScreen != null) {
             disposeCurrentScreen();
-            setCurrentScreen(screen);
-            getCurrentScreen().setVisible(true);
         }
+        setCurrentScreen(screen);
+        getCurrentScreen().setVisible(true);
     }
+
+    /**
+     * The method that updates the screen.
+     */
     public void updateCurrentScreenNoDispose(JFrame screen) {
         setCurrentScreen(screen);
         getCurrentScreen().setVisible(true);
     }
+
+    /**
+     * The method that disposes of the current screen.
+     */
     public void disposeCurrentScreen() {
         getCurrentScreen().dispose();
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher home screen.
+     */
     public void setResearcherStudyScreen(ResearcherStudyScreen screen) {
         if (researcherStudiesScreen != null) {
             researcherStudiesScreen.dispose();
         }
         researcherStudiesScreen = screen;
     }
+
+    /**
+     * The method sets the instance of the screen for a participant that is part of a study.
+     */
 
     public void setParticipantHomeScreen(ParticipantHomeScreen participantHomeScreen) {
         if (this.participantHomeScreen != null) {
@@ -136,6 +274,9 @@ public class ScreenManager {
         this.participantHomeScreen = participantHomeScreen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see the screen of a participant.
+     */
     public void setResearcherRequestParticipantScreen(@NotNull ResearcherRequestParticipantScreen screen) {
         if (this.researcherRequestParticipantScreen != null) {
             this.researcherRequestParticipantScreen.dispose();
@@ -144,6 +285,9 @@ public class ScreenManager {
         this.researcherRequestParticipantScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher create a study.
+     */
     public void setCreateStudyInputScreen(@NotNull CreateStudyInputScreen screen) {
         if (this.createStudyInputScreen != null) {
             this.createStudyInputScreen.dispose();
@@ -153,6 +297,11 @@ public class ScreenManager {
 
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see all the versions of a questionnaire answered
+     * by a participant.
+     */
+
     public void setQuestionnaireVersionedAnswerScreen(@NotNull AllVersionsScreen screen) {
         if (this.allVersionsScreen != null) {
             this.allVersionsScreen.dispose();
@@ -161,6 +310,9 @@ public class ScreenManager {
         this.allVersionsScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to create a questionnaire.
+     */
     public void setCreateQuestionnaireInputsScreen(@NotNull CreateQuestionnaireInputsScreen screen) {
         if (this.createQuestionnaireInputsScreen != null) {
             this.createQuestionnaireInputsScreen.dispose();
@@ -169,6 +321,9 @@ public class ScreenManager {
         this.createQuestionnaireInputsScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see the questionnaire.
+     */
     public void setCheckQuestionnaireScreen(@NotNull CheckQuestionnaireScreen screen) {
         if (this.checkQuestionnaireScreen != null) {
             this.checkQuestionnaireScreen.dispose();
@@ -177,6 +332,9 @@ public class ScreenManager {
         this.checkQuestionnaireScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see and specific version answer of a questionnaire.
+     */
     public void setCheckQuestionnaireVersionedAnswerScreen(@NotNull CheckVersionedAnswer screen) {
         if (this.checkVersionedAnswer != null) {
             this.checkVersionedAnswer.dispose();
@@ -184,6 +342,10 @@ public class ScreenManager {
         screen.setVisible(true);
         this.checkVersionedAnswer = screen;
     }
+
+    /**
+     * The method sets the instance of the screen for a researcher to choose the stratification of a study.
+     */
 
     public void setChooseStratificationScreen(@NotNull ChooseStratificationScreen screen) {
         if (this.chooseStratificationScreen != null) {
@@ -193,6 +355,9 @@ public class ScreenManager {
         this.chooseStratificationScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to edit a questionnaire.
+     */
     public void setEditQuestionnaireScreen(@NotNull EditQuestionnaireScreen screen) {
         if (this.editQuestionnaireScreen != null) {
             this.editQuestionnaireScreen.dispose();
@@ -201,6 +366,10 @@ public class ScreenManager {
         this.editQuestionnaireScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to edit a study.
+     */
+
     public void setEditStudyScreen(@NotNull EditStudyScreen screen) {
         if (this.editStudyScreen != null) {
             this.editStudyScreen.dispose();
@@ -208,6 +377,10 @@ public class ScreenManager {
         screen.setVisible(true);
         this.editStudyScreen = screen;
     }
+
+    /**
+     * The method sets the instance of the screen for a participant to see the questionnaire.
+     */
     public void setParticipantAnswerQuestionnairePanel(@NotNull UserAnswerQuestionnairesScreen screen) {
         if (this.userAnswerQuestionnairesScreen != null) {
             this.userAnswerQuestionnairesScreen.dispose();
@@ -216,6 +389,9 @@ public class ScreenManager {
         this.userAnswerQuestionnairesScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to modify the answer of a questionnaire.
+     */
     public void setResearcherEditQuestionnaireAnswersScreen(@NotNull EditQuestionnaireAnswers screen) {
         if (this.editQuestionnaireAnswers != null) {
             this.editQuestionnaireAnswers.dispose();
@@ -224,6 +400,9 @@ public class ScreenManager {
         this.editQuestionnaireAnswers = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see the data log of a study.
+     */
     public void setStudyDataLogScreen(StudyDataLogScreen screen) {
         if (this.studyDataLogScreen != null) {
             this.studyDataLogScreen.dispose();
@@ -232,6 +411,10 @@ public class ScreenManager {
         this.studyDataLogScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see the information of a researcher they want to
+     * add to the study.
+     */
     public void setShowResearcherInfoScreen(@NotNull ShowResearcherInfoScreen screen) {
         if (this.showResearcherInfoScreen != null) {
             this.showResearcherInfoScreen.dispose();
@@ -240,6 +423,10 @@ public class ScreenManager {
         this.showResearcherInfoScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see the information of a participant they want to
+     * add to the study.
+     */
     public void setShowParticipantInfoScreen(@NotNull ShowParticipantInfoScreen screen) {
         if (this.showParticipantInfoScreen != null) {
             this.showParticipantInfoScreen.dispose();
@@ -248,6 +435,9 @@ public class ScreenManager {
         this.showParticipantInfoScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see the groups to enroll a participant.
+     */
     public void setChooseGroupToAssignScreen(@NotNull ChooseGroupToAssignScreen screen) {
         if (this.chooseGroupToAssignScreen != null) {
             this.chooseGroupToAssignScreen.dispose();
@@ -256,6 +446,9 @@ public class ScreenManager {
         this.chooseGroupToAssignScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see the consent form of a study.
+     */
     public void setConsentFormScreen(@NotNull ConsentFormScreen screen) {
         if (this.consentFormScreen != null) {
             this.consentFormScreen.dispose();
@@ -264,6 +457,9 @@ public class ScreenManager {
         this.consentFormScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to create a consent form.
+     */
     public void setCreateConsentFormScreen(@NotNull CreateConsentFormScreen screen) {
         if (this.createConsentFormScreen != null) {
             this.createConsentFormScreen.dispose();
@@ -272,6 +468,9 @@ public class ScreenManager {
         this.createConsentFormScreen = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see the information of the consent form.
+     */
     public void setConsentFormScreenForReview(@NotNull ConsentFormScreenForReview screen) {
         if (this.consentFormScreenForReview != null) {
             this.consentFormScreenForReview.dispose();
@@ -280,6 +479,10 @@ public class ScreenManager {
         this.consentFormScreenForReview = screen;
     }
 
+    /**
+     * The method sets the instance of the screen for a researcher to see the information of the participant before
+     * assigning them a questionnaire.
+     */
     public void setShowParticipantInfoForQAssignmentScreen(@NotNull ShowParticipantInfoForQAssignment screen) {
         if (this.showParticipantInfoForQAssignment != null) {
             this.showParticipantInfoForQAssignment.dispose();
